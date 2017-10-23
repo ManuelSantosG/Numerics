f331e74f9c2bf12291db57750efa9a13c79df367
Manuel Santos <manuel.santosgut@gmail.com>
2017-10-23 17:38:07 +0100

    Ready to hand in, probably

--- linear_swe.py
+++ linear_swe.py
@@ -10,39 +10,42 @@ import numpy
 import math
 import matplotlib.pyplot as plt
 
+#############
+# Global Variables
+#############
+
 PPI=math.pi
 nt=60
 nx=60
-x=numpy.linspace(0,2*PPI,nx)
+x=numpy.linspace(0,2*PPI,nx) #linspace creates nx points in between 0 and 2pi
 t=numpy.linspace(0,1,nt)
 
+#Calculate timestep and space-step
 dt=t[1]-t[0]
 dx=x[1]-x[0]
 
-c=dt/dx
+c=dt/dx #Courant number
 g=1
 H=1
+
+#The matrices solu and solh store the solutions for u and h
 solu=numpy.zeros((nx,nt))
 solh=numpy.zeros((nx,nt))
 
+#Initial Condition (uncomment if no forcing is applied)
+#solh[int(nx/2),0]=1
 
-#Initial conditions
-#i=0
-#while i<nx:
-#    solu[i,0]=numpy.sin(x[i])
-#    i+=1
-
-#i=0
-#while i<nx:
-#    solh[i,0]=numpy.sin(x[i])
-#    i+=1
-
-solh[int(nx/2),0]=1
-
-#solh[:,0]=numpy.sin(x/2)
 
 
 print('Courant number: ', numpy.sqrt(g*H)*0.5*(dt/dx))
+
+
+
+#############
+# Core algorithm
+#############
+
+
 i=1
 while i<nt:
     j=1
@@ -52,23 +55,19 @@ while i<nt:
        
     j=1
     while j<nx-2:
-        if j==nx/2:
+        if j==nx/2: #This is the model for the forcing
            solu[j,i]=solu[j,i-1]-0.5*g*c*(solh[j+1,i-1]-solh[j-1,i-1]) + numpy.sin(8*t[i-1])
         else:
             solu[j,i]=solu[j,i-1]-0.5*g*c*(solh[j+1,i-1]-solh[j-1,i-1])
         solh[j,i]=solh[j,i-1]-0.5*H*c*(solu[j+1,i]-solu[j-1,i])
         j+=1
     
-    #Boundary Values
+    #Boundary Values: periodic boundary conditions.
     solu[0,i]=solu[0,i-1]-0.5*c*g*(solh[1,i-1]-solh[nx-2,i-1])
     solh[0,i]=solh[0,i-1]-0.5*c*H*(solu[1,i]-solu[nx-2,i])
     solu[nx-1,i]=solu[0,i]
     solh[nx-1,i]=solh[0,i]
     i+=1
 
-#i=0
-#plt.hold(True)
-#while i<nt:
-#    plt.plot(solh[:,i])
-#    i+=1
+#Solution at time 1/2
 plt.plot(solh[:,int(nt/2)])
--- linear_swe_collocated_implicit.py
+++ linear_swe_collocated_implicit.py
@@ -11,31 +11,52 @@ import math
 import matplotlib.pyplot as plt
 from scipy import linalg
 
+
+#############
+# Global Variables
+#############
+
 PPI=math.pi
 nt=40
 nx=10
 x=numpy.linspace(0,2*PPI,nx+1)
 t=numpy.linspace(0,10,nt)
 
+
+#Calculate timestep and space-step
 dt=t[1]-t[0]
 dx=x[1]-x[0]
 
-c=dt/dx
+c=dt/dx #Courant number
 g=1
 H=1
+
+
+
+#The matrices solu and solh store the solutions for u and h
 solu=numpy.zeros((nx+1,nt))
 solh=numpy.zeros((nx+1,nt))
-#solh[:,0]=numpy.sin(x-(numpy.pi /2)*numpy.ones(nx))
+
+
+#Initial Conditions
 solh[0:nx,0]=numpy.sin(x[0:nx]/2)
 
 
+
+#circ_vector is the vector that will generate the Toeplitz matrix
 circ_vector=numpy.zeros(nx)
 circ_vector[0]=1
 circ_vector[2]=-0.25*c*c*g*H
 circ_vector[nx-2]=0.25*c*c*g*H
 
 
-A=linalg.circulant(circ_vector)
+A=linalg.circulant(circ_vector) #circulant creates a Toeplitz matrix with main row circ_vector
+
+
+#############
+# Core algorithm, solution for h (only)
+#############
+
 
 i=1
 while i<nt:
@@ -50,12 +71,14 @@ while i<nt:
     print('eqb: ',eqb)
     solh[nx,i]=solh[0,i]
     i+=1
-    
-i=0
-plt.hold(True)
-while i<nt:
-    plt.plot(solh[:,i])
-    i+=1
+
+
+#Uncomment the following to see the sequence of solutions at each timestep    
+#i=0
+#plt.hold(True)
+#while i<nt:
+#    plt.plot(solh[:,i])
+#    i+=1
     
     
     
