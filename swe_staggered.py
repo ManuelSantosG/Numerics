e76a18fc1a57f1658a339d6e527a5701dbe19ba9
Manuel Santos <manuel.santosgut@gmail.com>
2017-11-11 16:58:02 +0000

    Finished algorithm Staggered

--- swe_staggered.py
+++ swe_staggered.py
@@ -20,17 +20,30 @@ c=0.1
 
 dt=c*dx
 
-t=numpy.arange(0,100,1)
+t=numpy.arange(0,10000,10)
 t=dt*t
 nt=len(t)
 
 
 u=numpy.sin(numpy.sin(numpy.pi*x))
 uold=numpy.sin(numpy.sin(numpy.pi*x))
+h=numpy.sin(numpy.sin(numpy.pi*x))
+hold=numpy.sin(numpy.sin(numpy.pi*x))
 
 
 for i in range(1,nt-1):
-    for j in range(0,nx-1)
-        u[]=uold[j-1]
+    for j in range(0,nx-1):
+        if j==nx-1:
+            u[j]=uold[j]-c*(hold[0]-hold[j])
+        else:
+            u[j]=uold[j]-c*(hold[j+1]-hold[j])
+    for j in range(0,nx-1):
+        if j==0:
+            h[0]=hold[0]-c*(u[0]-u[nx-1])
+        else:
+            h[j]=hold[j]-c*(u[j]-u[j-1])
+    uold=u.copy()
+    hold=h.copy()
+plt.plot(u)
     
     
