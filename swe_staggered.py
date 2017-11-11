989b9da08c686d9c4be413681e2123e326982adf
Manuel Santos <manuel.santosgut@gmail.com>
2017-11-11 17:09:22 +0000

    Comments on Staggered Algorithm.

--- swe_staggered.py
+++ swe_staggered.py
@@ -11,33 +11,38 @@ import math
 import matplotlib.pyplot as plt
 from scipy import linalg
 
+#space grid
 nx=100
 x=numpy.arange(0,nx,1)
 dx=1/nx
 x=dx*x
 
-c=0.1
+c=0.1 #Courant number
 
-dt=c*dx
+dt=c*dx #timestep
 
 t=numpy.arange(0,10000,10)
 t=dt*t
 nt=len(t)
 
-
+#initial conditions
 u=numpy.sin(numpy.sin(numpy.pi*x))
 uold=numpy.sin(numpy.sin(numpy.pi*x))
 h=numpy.sin(numpy.sin(numpy.pi*x))
 hold=numpy.sin(numpy.sin(numpy.pi*x))
 
-
+#Staggered algorithm, periodic BCs
 for i in range(1,nt-1):
+    #We first iterate to compute the new values of u =, which depend only on old values of h.
     for j in range(0,nx-1):
+        #u[0] is the value of u_{1/2}^{j} and so on...
+        #The only point of conflict is the end of the interval, whose value depends on hold[nx]=hold[0]
         if j==nx-1:
             u[j]=uold[j]-c*(hold[0]-hold[j])
         else:
             u[j]=uold[j]-c*(hold[j+1]-hold[j])
     for j in range(0,nx-1):
+        #The only point of conflict is at the beggining of the interval, whose value depends on u[-1/2]=u[nx-1]
         if j==0:
             h[0]=hold[0]-c*(u[0]-u[nx-1])
         else:
