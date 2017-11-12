3aa0d8bf1c675e765e5a9e746dd35241527f3236
Manuel Santos <manuel.santosgut@gmail.com>
2017-11-12 11:49:27 +0000

    Further modifications staggered

--- swe_staggered.py
+++ swe_staggered.py
@@ -12,11 +12,11 @@ import matplotlib.pyplot as plt
 from scipy import linalg
 
 #space grid
-nx=10
+nx=1000
 
 dx = 1./nx
 x = numpy.arange(0.,1.,dx)
-
+xu= x + dx*numpy.ones(len(x))
 c=0.1 #Courant number
 
 dt=c*dx #timestep
@@ -27,14 +27,14 @@ t=dt*t
 nt=len(t)
 
 #initial conditions
-u = numpy.sin(2*numpy.pi*x)
-h = numpy.sin(2*numpy.pi*x)
+u = numpy.sin(numpy.pi*xu)
+h = numpy.sin(numpy.pi*x)
 uold = u.copy()
 hold = h.copy()
 
 #Staggered algorithm, periodic BCs
 for i in range(1,nt):
-    #We first iterate to compute the new values of u =, which depend only on old values of h.
+    #We first iterate to compute the new values of u, which depend only on old values of h.
     for j in range(0,nx):
         #u[0] is the value of u_{1/2}^{j} and so on...
         #The only point of conflict is the end of the interval, whose value depends on hold[nx]=hold[0]
@@ -44,10 +44,7 @@ for i in range(1,nt):
             u[j]=uold[j]-c*(hold[j+1]-hold[j])
     for j in range(0,nx):
         #The only point of conflict is at the beggining of the interval, whose value depends on u[-1/2]=u[nx-1]
-        if j==0:
-            h[0]=hold[0]-c*(u[0]-u[nx-1])
-        else:
-            h[j]=hold[j]-c*(u[j]-u[j-1])
+        h[j]=hold[j]-c*(u[j]-u[(j-1)%nx])
     uold=u.copy()
     hold=h.copy()
 plt.plot(x, h, label='h')
